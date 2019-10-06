6edc9f4b47730187e731f9ca954562fbcc4a4403
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-10-06 16:10:38 +0000

    Automatically backed up by Learn

--- spec/list_tags_spec.rb
+++ spec/list_tags_spec.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 RSpec.describe 'index.html' do
-  it 'contains a <ul> tag with the correct <li> tags' do
+  xit 'contains a <ul> tag with the correct <li> tags' do
     ul = parsed_html.search('ul').first
     expect(ul).to_not be_nil, "The main <ul> tag is missing!"
     expect(html_file_contents).to include('</ul>'), "Don't forget to include a closing </ul>"
